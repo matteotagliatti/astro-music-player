aa89197f1a93ef9c3b2d8275853db38ac2209d01
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-09-16 16:32:35 +0200

    more albums, no title

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -2,7 +2,7 @@ const client_id = import.meta.env.SPOTIFY_CLIENT_ID;
 const client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN;
 const token_endpoint = `https://accounts.spotify.com/api/token`;
-const fav_albums_endpoint = `https://api.spotify.com/v1/albums?ids=7btiyhWzUfzxN3ijSiBpC8%2C1C30LhZB9I48LdpVCRRYvq%2C1cCAb1vN8uUsdfEylVmTLs%2C5EbpxRwbbpCJUepbqVTZ1U&market=IT`;
+const fav_albums_endpoint = `https://api.spotify.com/v1/albums?ids=7btiyhWzUfzxN3ijSiBpC8%2C1C30LhZB9I48LdpVCRRYvq%2C1cCAb1vN8uUsdfEylVmTLs%2C5EbpxRwbbpCJUepbqVTZ1U%2C7bvmGyFDwpHNRRRZJ0AHvn%2C1pOl0KEC1iQnA6F0XxV4To%2C6nfJMRoIjyRwk3ZTHNm0PY%2C6vN8o7jyIAJvFPqC0vxxmm&market=IT`;
 
 type Headers = {
   headers: {
